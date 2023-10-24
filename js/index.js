e2342aa71c35ce3c6e769b752a1ffd0571711c84
kelvin abche <kelvinabache12@gmail.com>
2023-10-23 20:13:53 -0400

    haciendo las ideas sobre el proyecto

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/**
+ * lista de cosas por hacer:
+ * 1) Hacer una class padre que tenga la api
+ * 2) Intentar hacer una herencia y recibir los datos
+ * 3) hacer una estructura muy basica para mostrar los datos json
+ */
